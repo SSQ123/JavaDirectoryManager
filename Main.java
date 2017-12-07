7c21329dba17c1c364d6453bcb20692651c3995a
SSQ <dos1234567890a@gmail.com>
2017-12-07 10:02:44 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+public class Main extends JFrame{
+public static void main(args String[])
+{
+this.setSize(800,600);
+this.setVisible(true);
+this.add(MenuBar,BorderLayou.NORTH);
+//Should be on top
+this.setTitle("Java Filemanager beta .1");
+}
+//Coded by SSQ aka Super Saiyan Quilava
+//Copyright Dec 2017
+//GNU Gnu Not Unix 3.0 license idk if it's spelled right
+//See license.txt for more info
+
+
+
+
+
+
+
+
+
---
+++ MenuBar.java
@@ -0,0 +1,10 @@
+Public class MenuBar extends JPanel
+{
+BoldButton = new JButton("B");
+this.add(BoldButton);
+Font sizes = JScrollBox(FontSizes[]);
+Fontsizes[]={"12","14","16","18","24","32","48","72"};
+this.add(FontSizes);
+Color Chooser = new JButton("Colors");
+
+
