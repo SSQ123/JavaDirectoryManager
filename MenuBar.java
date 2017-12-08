cc8b65976c151ac29defbd020dff8a5fa945602b
SSQ <dos1234567890a@gmail.com>
2017-12-08 11:18:18 -0500

    Update MenuBar.java
    Wrong type of application was gonna be a word processor until I fixed the lines. XD

--- MenuBar.java
+++ MenuBar.java
@@ -1,10 +1,6 @@
 Public class MenuBar extends JPanel
 {
-BoldButton = new JButton("B");
-this.add(BoldButton);
-Font sizes = JScrollBox(FontSizes[]);
-Fontsizes[]={"12","14","16","18","24","32","48","72"};
-this.add(FontSizes);
-Color Chooser = new JButton("Colors");
-
+FileMenu = new JButton("File");
+This.add(FileMenu);
+}
 
